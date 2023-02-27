c03340dea421c315e470cceed56984605087c764
“sheila” <sheilavangeusau@gmail.com>
2023-02-27 18:54:20 +0100

    added description field to main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
-            description: "very cool socks with the Vue logo. Unisex and one size fits all. Yes really! You will love them!"
+            product: 'Socks',
+            description: 'Very cool socks with the Vue logo. Unisex and one size fits all. Yes really! You will love them!'
         }
     }
 })
