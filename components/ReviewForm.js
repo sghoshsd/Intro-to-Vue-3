4e56d200fc4abb23ed570808aece5dac581a90ce
“sheila” <sheilavangeusau@gmail.com>
2023-03-05 21:07:24 +0100

    added<

--- components/ReviewForm.js
+++ components/ReviewForm.js
@@ -1,7 +1,7 @@
 app.component('review-form', {
     template:
     /*html*/
-    'form class="review-form" @submit.prevent="onSubmit">
+    '<form class="review-form" @submit.prevent="onSubmit">
     <h3>Leave a review'</h3>
     <label for="name">Name:</label>
     <input id="name" v-model="review">
